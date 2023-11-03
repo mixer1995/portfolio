4a016729d822d4ae8c8dd80018d50bf93ec66a7f
Maksym Shkolnyi <mixer19995@gmail.com>
2023-11-03 13:41:31 +0200

    final touches

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,37 +1,29 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
   {
     id: 1,
-    title: "Game Hub",
-    img: "/gameHub.png",
+    title: "Game Hub (ReactJS)",
+    img: ["/gameHub.png"],
     desc: ["I've developed a React project that serves as a versatile game library. Users can easily sort and manage their games by genre, platform, and search queries. The project fetches data from an external API, ensuring that the latest games, genres, and platforms are readily available. To maintain a seamless user experience, loading skeletons are displayed during data retrieval.", "In addition to its practical features, the project offers a choice between light and dark themes, allowing users to tailor their interface to their preferences. What sets this project apart is its full responsiveness, ensuring that the application adapts smoothly to different screen sizes and devices, making it accessible and enjoyable for users across the board."],
     target: "https://mixer1995.github.io/game_hub/",
     srcTarget: "https://github.com/mixer1995/game_hub"
   },
   {
     id: 2,
-    title: "Next.js Blog",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum."],
-    target: "",
+    title: "Employee Knowledge Hub (Php)",
+    img: ["/leanP-01.png", "/leanP-02.png", "/leanP-03.png", "/leanP-04.png", "/leanP-05.png"],
+    desc: ["The Employee Knowledge Hub is a one-stop platform, developed in-house, that enables employees to access learning materials, take tests, review their results, and sign essential documents. Empower your workforce with this intuitive and efficient tool for continuous learning and development!", "In addition - fully responsive design is implemented, so that employees would be able to use this portal on any device."],
+    target: "http://man.e-pienas.lt/index.php",
     srcTarget: ""
   },
   {
     id: 3,
-    title: "Vanilla JS App",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum."],
-    target: "",
-    srcTarget: ""
-  },
-  {
-    id: 4,
-    title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum."],
+    title: "OEE tracking site (Php)",
+    img: ["/oeeP-01.png","/oeeP-02.png","/oeeP-03.png"],
+    desc: ["Elevate your manufacturing operations to new heights with our state-of-the-art OEE tracking site. Our platform offers real-time data visualization that provides valuable insights into your production processes, enabling you to identify and address bottlenecks, downtime, and inefficiencies on the fly. By harnessing the power of real-time data, you can make informed decisions that lead to significant improvements in productivity, quality, and overall operational effectiveness. Streamline your manufacturing processes, optimize resource utilization, and boost your bottom line with our OEE tracking site."],
     target: "",
     srcTarget: ""
   },
@@ -45,13 +37,25 @@ const Single = ({ item }) => {
   });
 
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % item.img.length);
+    }, 5000); // Change images every 3 seconds (adjust as needed)
+
+    return () => clearInterval(interval);
+  }, [item.img.length]);
 
   return (
     <section >
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+            {/* <img src={item.img[currentIndex]} alt="" /> */}
+            {item.img.map((image, i) => (
+              <img key={i} src={image} alt="" style={{ opacity: i === currentIndex ? 1 : 0 }} />
+            ))}
           </div>
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
